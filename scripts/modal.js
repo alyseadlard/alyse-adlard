8aec0605852ea3d98702968b0337b52482d9e75d
alyseadlard <alyseadlard@gmail.com>
2015-10-15 16:26:44 -0400

    Port current site to git

---
+++ scripts/modal.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.bxslider1').bxSlider({
+    slideWidth: 550,
+    minSlides: 1,
+    maxSlides: 1,
+    moveSlides: 1,
+    slideMargin: 0,
+    adaptiveHeight: true
+  });
+});
+
